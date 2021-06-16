a4d70996bb2fd926ebca09169e51ef163684c594
rakib <phi.rakib@gmail.com>
2021-06-16 11:29:09 +0600

    created adding node functionality in the linked list. Checked if the head is null then set new node as a head otherwise travese to the last node and add new node as a next node of the current last node.

--- linked-list/singly-linked-list.cpp
+++ linked-list/singly-linked-list.cpp
@@ -8,6 +8,26 @@ struct node {
 
 node *head = NULL;
 
+void addNode(int value) {
+  if(head == NULL) {
+    head = new node();
+    head->value = value;
+    head->next = NULL;
+    return;
+  }
+  
+  node *currentNode = head;
+  while(currentNode->next != NULL) {
+    currentNode = currentNode->next;
+  }
+
+  node *newNode = new node();
+  newNode->value = value;
+  newNode->next = NULL;
+
+  currentNode->next = newNode;
+}
+
 int main() {
   
   return 0;
